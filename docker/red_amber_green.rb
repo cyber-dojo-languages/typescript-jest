56c7201a8d50aaa666bad674a53b7ee357adcd79
Jon Jagger <jon@jaggersoft.com>
2020-09-02 17:49:04 +0100

    Add initial red_amber_green.rb regex lambda

--- docker/red_amber_green.rb
+++ docker/red_amber_green.rb
@@ -1,8 +1,14 @@
 
 lambda { |stdout,stderr,status|
+  if status === 0
+    return :green
+  end
   output = stdout + stderr
-  js_hint_pattern = /^(\d+) error(s?)/
-  if js_hint_pattern.match(output)
+  #js_hint_pattern = /^(\d+) error(s?)/
+  #if js_hint_pattern.match(output)
+  #  return :amber
+  #end
+  if output.include?('Test suite failed to run')
     :amber
   elsif /^FAIL/.match(output)
     :red
