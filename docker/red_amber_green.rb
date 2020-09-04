bc3b3df528355881ff9bf295fada3bfe67730a37
Jon Jagger <jon@jaggersoft.com>
2020-09-04 09:02:28 +0100

    Delete dead code

--- docker/red_amber_green.rb
+++ docker/red_amber_green.rb
@@ -4,10 +4,6 @@ lambda { |stdout,stderr,status|
     return :green
   end
   output = stdout + stderr
-  #js_hint_pattern = /^(\d+) error(s?)/
-  #if js_hint_pattern.match(output)
-  #  return :amber
-  #end
   if output.include?('Test suite failed to run')
     :amber
   elsif /^FAIL/.match(output)
